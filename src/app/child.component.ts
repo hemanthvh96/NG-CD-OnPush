258199b6e2b213bf5b264b804ba3f11e69f76b80
hemanthvh96 <hemanthvennelakanti@gmail.com>
2022-08-02 15:47:44 +0530

    1 file modified

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -14,13 +14,22 @@ import {
   styleUrls: ['child.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChildComponent implements DoCheck {
+export class ChildComponent implements DoCheck, OnInit {
   @Input() players$;
   players;
 
   constructor(private cdr: ChangeDetectorRef) {}
 
+  // Both the below implementations works !!!
+
   ngDoCheck() {
+    // this.players$.subscribe((data) => {
+    //   this.players = data;
+    //   this.cdr.markForCheck();
+    // });
+  }
+
+  ngOnInit() {
     this.players$.subscribe((data) => {
       this.players = data;
       this.cdr.markForCheck();
